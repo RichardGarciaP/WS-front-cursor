9bbd8421d6477d7f84838b00fdf83b9004fda33f
RichardGarciaP <68579638+RichardGarciaP@users.noreply.github.com>
2024-04-01 16:32:16 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+// import './App.css'
+import Home from './Home'
+import { Login } from './components/Login'
+
+function App() {
+  const [username, setUsername] = useState("")
+
+  return  username ? 
+     ( 
+        <Home  username={username}/>    
+      ) : 
+      (    
+        <Login onSubmit={setUsername}/>
+      );    
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import throttle from "lodash.throttle";
+import React from "react";
+import useWebSocket from "react-use-websocket";
+
+const Home = ({ username }) => {
+  const WS_URL = "ws://localhost:8000";
+
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
+
+  const THROTTLE = 1000; // 1s;
+  const sendJsonMessageThrottled = throttle(sendJsonMessage, THROTTLE);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      sendJsonMessageThrottled({ x: e.clientX, y: e.clientY });
+    });
+
+    return () => {
+      window.removeEventListener("mousemove");
+    };
+  }, []);
+
+  return <h1>Hello, {username}</h1>;
+};
+
+export default Home;
